7a9a0e2d81c4f4cd4a52b0ea687be1b59526e796
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-09-18 15:52:33 +0300

    5. Day 5  JavaScript Side Menu Tutorial

---
+++ 10 Projects In 10 Days/5. Day 5  JavaScript Side Menu Tutorial/app.js
@@ -0,0 +1,5 @@
+const menuToggle = document.querySelector("#menu-toggle");
+const menuList = document.querySelector("#menu-list");
+menuToggle.addEventListener("click", () => {
+  menuList.classList.toggle("active");
+});
